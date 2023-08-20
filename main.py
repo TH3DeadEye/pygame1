a7e8a243e874925bce75a477fac2dee053fdf1f0
Arman <armanmilani84@gmail.com>
2023-08-20 17:58:46 +0330

    first commit

--- main.py
+++ main.py
@@ -4,11 +4,16 @@ from pygame.image import load
 pygame.init()
 WIDGHT, HEIGHT = 900 , 450
 
-pygame.display.set_caption('First Game')
+pygame.display.set_caption('Super Mario Bros Remake')
 screen = pygame.display.set_mode((WIDGHT, HEIGHT))
 clock = pygame.time.Clock()
 
-test = load('Vision.png')
+#loading player
+marrio_surface = load('Sprite Sheet/Mario/Untitled-1.png').convert()
+marrio_rect = marrio_surface.get_rect(center = (450 , 275))
+
+
+
 #game loop
 while True:
     #quiting the game
@@ -16,10 +21,23 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                print('Up')
+            if event.key == pygame.K_DOWN:
+                print("down")
+            if event.key == pygame.K_RIGHT:
+                print('right')
+            if event.key == pygame.K_LEFT:
+                print("left")
+    screen.blit(marrio_surface, marrio_rect)
+
+
+
+
+
 
     pygame.display.update()
     clock.tick(60)
 
-    screen.blit(test, (0,0))
-
-    print(int(clock.get_fps()))
+    #print(int(clock.get_fps()))
